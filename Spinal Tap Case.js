265fce641f64caf0a960c95d9a2a170074487595
Ana Toma <ana.toma92@gmail.com>
2017-03-24 13:31:57 +0200

    Create Spinal Tap Case.js
    Convert a string to spinal case. Spinal case is all-lowercase-words-joined-by-dashes.
    spinalCase("This Is Spinal Tap") should return "this-is-spinal-tap".
    spinalCase("thisIsSpinalTap") should return "this-is-spinal-tap".
    spinalCase("The_Andy_Griffith_Show") should return "the-andy-griffith-show".
    spinalCase("Teletubbies say Eh-oh") should return "teletubbies-say-eh-oh".
    spinalCase("AllThe-small Things") should return "all-the-small-things".

---
+++ Spinal Tap Case.js
@@ -0,0 +1,11 @@
+function spinalCase(str) {
+  
+  str = str.replace(/([a-z])([A-Z])/g, '$1 $2');
+  str = str.replace(/\s+|_+/g, "-").toLowerCase();
+  
+  return str;
+}
+
+spinalCase('This Is Spinal Tap');
+spinalCase("thisIsSpinalTap");
+spinalCase("The_Andy_Griffith_Show");
