91c63c67b3b61fe1a2026737f30b4f452cc21f22
Ana Toma <ana.toma92@gmail.com>
2017-03-26 20:41:08 +0300

    Create DNA Pairing.js
    The DNA strand is missing the pairing element. Take each character, get its pair, and return the results as a 2d array.
    
    Base pairs are a pair of AT and CG. Match the missing element to the provided character.
    
    Return the provided character as the first element in each array.
    
    For example, for the input GCG, return [["G", "C"], ["C","G"],["G", "C"]]
    
    The character and its pair are paired up in an array, and all the arrays are grouped into one encapsulating array.
    
    pairElement("ATCGA") should return [["A","T"],["T","A"],["C","G"],["G","C"],["A","T"]].
    pairElement("TTGAG") should return [["T","A"],["T","A"],["G","C"],["A","T"],["G","C"]].
    pairElement("CTCTA") should return [["C","G"],["T","A"],["C","G"],["T","A"],["A","T"]].

---
+++ DNA Pairing.js
@@ -0,0 +1,23 @@
+function pairElement(str) {
+  str=str.split("");
+  var newArr=[];
+  for(var i=0;i<str.length;i++){
+    switch(str[i]) {
+    case "A":
+        newArr.push(["A","T"]);
+        break;
+    case "T":
+        newArr.push(["T","A"]);
+        break;
+    case "C":
+        newArr.push(["C","G"]);
+        break;
+    case "G":
+        newArr.push(["G","C"]);
+        break;
+  }
+  }
+  return newArr;
+}
+
+pairElement("ATCGA");
